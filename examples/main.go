3260bb49ac67d0f8247bd2c6d3d9406fa4c53ba8
Steve Partridge <srpartridge@gmail.com>
2016-02-05 19:16:39 -0800

    basic example

---
+++ examples/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stevepartridge/geocodio"
+)
+
+func main() {
+
+	Geocodio, err := geocodio.NewGeocodio("YOUR_API_KEY")
+	if err != nil {
+		panic(err)
+	}
+
+	result, err := Geocodio.Geocode("42370 Bob Hope Dr, Rancho Mirage, CA")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Geocode Result %v", result)
+
+	resultReverse, err := Geocodio.ReverseGeocode(38.9002898, -76.9990361)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Reverse Geocode Result %v", resultReverse)
+}
