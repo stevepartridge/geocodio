aeb3627c85e083596e8fa09ad2db692297054ed5
Steve Partridge <srpartridge@gmail.com>
2020-05-16 14:29:28 -0700

    Add zip4 models

--- models.go
+++ models.go
@@ -55,6 +55,7 @@ type Location struct {
 // Fields
 type Fields struct {
 	Timezone                  Timezone                  `json:"timezone,omitempty"`
+	Zip4                      Zip4                      `json:"zip4,omitempty"`
 	CongressionalDistrict     CongressionalDistrict     `json:"congressional_district,omitempty"`  // v1.0
 	CongressionalDistricts    []CongressionalDistrict   `json:"congressional_districts,omitempty"` // v1.1+
 	StateLegislativeDistricts StateLegislativeDistricts `json:"state_legislative_districts,omitempty"`
@@ -80,6 +81,63 @@ type Timezone struct {
 	UTCOffset    int    `json:"utc_offset"`
 	ObservesDST  bool   `json:"observes_dst"`
 	Source       string `json:"source"`
+}
+
+// Zip4 based on this payload example
+/*
+{
+	"record_type": {
+		"code": "S",
+		"description": "Street"
+	},
+	"carrier_route": {
+		"id": "C007",
+		"description": "City Delivery"
+	},
+	"building_or_firm_name": null,
+	"plus4": [
+		"2890"
+	],
+	"zip9": [
+		"22201-2890"
+	],
+	"government_building": null,
+	"facility_code": {
+		"code": "P",
+		"description": "Post Office"
+	},
+	"city_delivery": true,
+	"valid_delivery_area": true,
+	"exact_match": true
+}
+*/
+
+type Zip4 struct {
+	RecodeType         RecordType   `json:"record_type,omitempty"`
+	CarrierRoute       CarrierRoute `json:"carrier_route,omitempty"`
+	BuildingOrFirmName string       `json:"building_or_firm_name,omitempty"`
+	Plus4              []string     `json:"plus4,omitempty"`
+	Zip9               []string     `json:"zip9,omitempty"`
+	GovermentBuilding  string       `json:"government_building,omitempty"`
+	FacilityCode       FacilityCode `json:"facility_code,omitempty"`
+	CityDelivery       bool         `json:"city_delivery,omitempty"`
+	ValidDeliveryArea  bool         `json:"valid_delivery_area,omitempty"`
+	ExactMatch         bool         `json:"exact_match,omitempty"`
+}
+
+type RecordType struct {
+	Code        string `json:"code"`
+	Description string `json:"description"`
+}
+
+type CarrierRoute struct {
+	ID          string `json:"id"`
+	Description string `json:"description"`
+}
+
+type FacilityCode struct {
+	Code        string `json:"code"`
+	Description string `json:"description"`
 }
 
 // Congressional District field
