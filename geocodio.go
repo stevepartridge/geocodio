8e4f6b4578486663193ef76d6aa6be42bbbc0709
Steve Partridge <srpartridge@gmail.com>
2020-08-05 22:14:03 -0700

    Add simplified New and support env value

--- geocodio.go
+++ geocodio.go
@@ -1,23 +1,60 @@
 package geocodio
 
 import (
-	"errors"
+	"os"
+	"strings"
 )
 
 const (
 	// GeocodioAPIBaseURLv1 is the Geocod.io Base URL
-	GeocodioAPIBaseURLv1 = "https://api.geocod.io/v1.5"
+	GeocodioAPIBaseURLv1 = "https://api.geocod.io/v1.6"
 )
 
-// NewGeocodio is a helper to create new Geocodio pointer
+// Geocodio is the base struct
+type Geocodio struct {
+	APIKey string
+}
+
+type Input struct {
+	AddressComponents Components `json:"address_components"`
+	FormattedAddress  string     `json:"formatted_address"`
+}
+
+// New creates a new Geocodio instance based on an API key in either the environment
+// or passed in as the first string value
+func New(apiKey ...string) (*Geocodio, error) {
+
+	key := os.Getenv(EnvGeocodioAPIKey)
+	if strings.TrimSpace(key) == "" {
+		key = os.Getenv(EnvOldAPIKey)
+	}
+	if len(apiKey) == 0 && strings.TrimSpace(key) == "" {
+		return nil, ErrMissingApiKey
+	}
+
+	if len(apiKey) == 1 {
+		key = apiKey[0]
+	}
+
+	if strings.TrimSpace(key) == "" {
+		return nil, ErrMissingApiKey
+	}
+
+	return NewGeocodio(key)
+}
+
+// NewGeocodio is a helper to create new Geocodio reference
+// since 1.6+ this is kept for backwards compatiblity
+// after 2+ this will be deprecated
 func NewGeocodio(apiKey string) (*Geocodio, error) {
 
 	if apiKey == "" {
-		return nil, errors.New("apiKey is missing")
+		return nil, ErrMissingApiKey
 	}
 
-	newGeocodio := new(Geocodio)
-	newGeocodio.APIKey = apiKey
+	g := Geocodio{
+		APIKey: apiKey,
+	}
 
-	return newGeocodio, nil
+	return &g, nil
 }
