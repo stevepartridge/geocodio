0ebe799d77dbd23f451c48c10a449cebe0eee8b4
Steve Partridge <srpartridge@gmail.com>
2020-08-05 22:12:43 -0700

    Add error for invalid API key

--- errors.go
+++ errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	// ErrReverseGecodeMissingLatLng error when a lat/lng is not provided
 	ErrReverseGecodeMissingLatLng = errors.New("Latitude and longitude must not be empty")
+	ErrMissingApiKey              = errors.New("Missing or empty API key")
 )
