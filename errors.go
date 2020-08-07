ffcd153fea4114e7c6d71ce6d5e326d65e429ccb
Steve Partridge <srpartridge@gmail.com>
2020-08-07 00:02:10 -0700

    Add error messages

--- errors.go
+++ errors.go
@@ -6,4 +6,6 @@ var (
 	// ErrReverseGecodeMissingLatLng error when a lat/lng is not provided
 	ErrReverseGecodeMissingLatLng = errors.New("Latitude and longitude must not be empty")
 	ErrMissingApiKey              = errors.New("Missing or empty API key")
+	ErrAddressIsEmpty             = errors.New("Address must not be empty")
+	ErrBatchAddressesIsEmpty      = errors.New("At least one address is required for batch query")
 )
