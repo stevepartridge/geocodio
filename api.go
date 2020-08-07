c9bf056eb90033c0021d1e840696e8204ce62372
Steve Partridge <srpartridge@gmail.com>
2020-08-07 00:02:54 -0700

    Initial move to flexible api call usage

--- api.go
+++ api.go
@@ -3,6 +3,7 @@ package geocodio
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 
 	// "fmt"
 	"io/ioutil"
@@ -10,6 +11,11 @@ import (
 	"net/url"
 	"strings"
 	"time"
+)
+
+const (
+	MethodGet  = "GET"
+	MethodPost = "POST"
 )
 
 // Call uses basic (GET) method to make a request to the API
@@ -47,6 +53,71 @@ func (g *Geocodio) Call(path string, query map[string]string) (GeocodeResult, er
 	result := GeocodeResult{}
 
 	result.Debug.RequestedURL = _url
+	result.Debug.Status = resp.Status
+	result.Debug.StatusCode = resp.StatusCode
+	result.Debug.RawResponse = body
+
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return result, err
+	}
+
+	return result, err
+}
+
+func (g *Geocodio) get(path string, query map[string]string) (GeocodeResult, error) {
+	return g.call(MethodGet, path, nil, query)
+}
+func (g *Geocodio) post(path string, payload interface{}, query map[string]string) (GeocodeResult, error) {
+	return g.call(MethodPost, path, payload, query)
+}
+
+func (g *Geocodio) call(method, path string, payload interface{}, query map[string]string) (GeocodeResult, error) {
+
+	if strings.Index(path, "/") != 0 {
+		return GeocodeResult{}, errors.New("Path must start with a forward slash: ' / ' ")
+	}
+
+	rawURL := GeocodioAPIBaseURLv1 + path + "?api_key=" + g.APIKey
+
+	if query != nil {
+		for k, v := range query {
+			rawURL = fmt.Sprintf("%s&%s=%s", rawURL, k, url.QueryEscape(v))
+		}
+	}
+
+	timeout := time.Duration(10 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return GeocodeResult{}, nil
+	}
+
+	req := http.Request{
+		Method: method,
+		URL:    u,
+	}
+
+	resp, err := client.Do(&req)
+
+	if err != nil {
+		return GeocodeResult{}, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return GeocodeResult{}, err
+	}
+
+	result := GeocodeResult{}
+
+	result.Debug.RequestedURL = u.String()
 	result.Debug.Status = resp.Status
 	result.Debug.StatusCode = resp.StatusCode
 	result.Debug.RawResponse = body
