6f4f3d9a642d2891a34eca0460216c43c781f909
Steve Partridge <srpartridge@gmail.com>
2020-08-08 14:47:16 -0700

    Refactor API helpers for better flexibility

--- api.go
+++ api.go
@@ -1,6 +1,7 @@
 package geocodio
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -14,60 +15,16 @@ import (
 )
 
 const (
-	MethodGet  = "GET"
+	// MethodGet constant
+	MethodGet = "GET"
+	// MethodPost constant
 	MethodPost = "POST"
 )
-
-// Call uses basic (GET) method to make a request to the API
-func (g *Geocodio) Call(path string, query map[string]string) (GeocodeResult, error) {
-
-	if strings.Index(path, "/") != 0 {
-		return GeocodeResult{}, errors.New("Path must start with a forward slash: ' / ' ")
-	}
-
-	_url := GeocodioAPIBaseURLv1 + path + "?api_key=" + g.APIKey
-
-	for k, v := range query {
-		_url = _url + "&" + k + "=" + url.QueryEscape(v)
-	}
-
-	timeout := time.Duration(10 * time.Second)
-	client := http.Client{
-		Timeout: timeout,
-	}
-
-	resp, err := client.Get(_url)
-
-	if err != nil {
-		return GeocodeResult{}, err
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		return GeocodeResult{}, err
-	}
-
-	result := GeocodeResult{}
-
-	result.Debug.RequestedURL = _url
-	result.Debug.Status = resp.Status
-	result.Debug.StatusCode = resp.StatusCode
-	result.Debug.RawResponse = body
-
-	err = json.Unmarshal(body, &result)
-	if err != nil {
-		return result, err
-	}
-
-	return result, err
-}
 
 func (g *Geocodio) get(path string, query map[string]string) (GeocodeResult, error) {
 	return g.call(MethodGet, path, nil, query)
 }
+
 func (g *Geocodio) post(path string, payload interface{}, query map[string]string) (GeocodeResult, error) {
 	return g.call(MethodPost, path, payload, query)
 }
@@ -96,13 +53,34 @@ func (g *Geocodio) call(method, path string, payload interface{}, query map[stri
 		return GeocodeResult{}, nil
 	}
 
+	if query != nil {
+		for k, v := range query {
+			if u.Query().Get(k) != "" {
+				u.Query().Set(k, v)
+				continue
+			}
+			u.Query().Add(k, v)
+		}
+	}
+
 	req := http.Request{
 		Method: method,
 		URL:    u,
+		Header: http.Header{},
+	}
+
+	if payload != nil {
+		body, err := json.Marshal(payload)
+		if err != nil {
+			return GeocodeResult{}, err
+		}
+
+		req.Body = ioutil.NopCloser(bytes.NewReader(body))
+
+		req.Header.Add("Content-Type", "application/json")
 	}
 
 	resp, err := client.Do(&req)
-
 	if err != nil {
 		return GeocodeResult{}, err
 	}
@@ -110,7 +88,6 @@ func (g *Geocodio) call(method, path string, payload interface{}, query map[stri
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
-
 	if err != nil {
 		return GeocodeResult{}, err
 	}
@@ -125,6 +102,14 @@ func (g *Geocodio) call(method, path string, payload interface{}, query map[stri
 	err = json.Unmarshal(body, &result)
 	if err != nil {
 		return result, err
+	}
+
+	if len(result.Results) > 0 {
+		if result.Results[0].Error != nil {
+			if result.Results[0].Error.Message != "" {
+				return result, errors.New(result.Results[0].Error.Message)
+			}
+		}
 	}
 
 	return result, err
