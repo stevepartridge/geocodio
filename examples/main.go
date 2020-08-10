5de0ac436d4a775f58c1c9a73154553054612ff1
steve <srpartridge@gmail.com>
2020-08-10 09:55:19 -0700

    Update examples to reflect method name changes

--- examples/main.go
+++ examples/main.go
@@ -8,18 +8,18 @@ import (
 
 func main() {
 
-	gc, err := geocodio.NewGeocodio("YOUR_API_KEY")
+	gc, err := geocodio.New("YOUR_API_KEY")
 	if err != nil {
 		panic(err)
 	}
 
-	result, err := Geocodio.Geocode("42370 Bob Hope Dr, Rancho Mirage, CA")
+	result, err := gc.Geocode("42370 Bob Hope Dr, Rancho Mirage, CA")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("Geocode Result %v", result)
 
-	resultReverse, err := gc.ReverseGeocode(38.9002898, -76.9990361)
+	resultReverse, err := gc.Reverse(38.9002898, -76.9990361)
 	if err != nil {
 		panic(err)
 	}
